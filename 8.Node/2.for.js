6b7619b80443936f64b8d14beabb6d3f9383244a
hyein310 <kanghyein310@naver.com>
2025-04-11 12:45:00 +0900

    nodejs start

---
+++ 8.Node/2.for.js
@@ -0,0 +1,7 @@
+function myloop() {
+  for (let i = 0; i < 3; i++) {
+    console.log(`현재 인덱스의 위치는 ${i}`);
+  }
+}
+
+myloop;
---
+++ 8.Node/3.gugudan.js
@@ -0,0 +1,7 @@
+function gugudan() {
+  for (let i = 2; i <= 9; i++) {
+    console.log(`== ${i}단 ==`);
+    for (let j = 1; j <= 9; j++) console.log(`${i} x ${j} = ${i * j}`);
+  }
+}
+gugudan();
