7749ecb87303bee0736675781b72aba5ce01af2f
hyein310 <kanghyein310@naver.com>
2025-04-13 05:52:40 +0900

    triangle prc

---
+++ Practice/triangle.js
@@ -0,0 +1,82 @@
+function leftTrianlge() {
+  for (let i = 0; i < 5; i++) {
+    for (let j = 1; j <= i + 1; j++) {
+      // 줄 바꿈 없이 출력
+      process.stdout.write("*");
+    }
+    process.stdout.write("\n");
+  }
+  console.log("\n");
+}
+
+function leftIntervertTriangle() {
+  for (let i = 0; i < 5; i++) {
+    for (let j = 0; j <= 4 - i; j++) {
+      process.stdout.write("*");
+    }
+    process.stdout.write("\n");
+  }
+  console.log("\n");
+}
+
+function rigthTriangle() {
+  for (let i = 0; i < 5; i++) {
+    for (let j = 0; j < 4 - i; j++) {
+      process.stdout.write(" ");
+    }
+    for (let k = 1; k <= i + 1; k++) {
+      process.stdout.write("*");
+    }
+    process.stdout.write("\n");
+  }
+  console.log("\n");
+}
+
+function rigthIntervertTriangle() {
+  for (let i = 0; i < 5; i++) {
+    for (let k = 1; k < i + 1; k++) {
+      process.stdout.write(" ");
+    }
+    for (let j = 0; j <= 4 - i; j++) {
+      process.stdout.write("*");
+    }
+
+    process.stdout.write("\n");
+  }
+  console.log("\n");
+}
+
+function doublesideTrianlge() {
+  let cnt = 1;
+  for (let i = 0; i < 5; i++) {
+    for (let j = 0; j < 4 - i; j++) {
+      process.stdout.write(" ");
+    }
+    for (let k = 0; k < i * 2 + 1; k++) {
+      process.stdout.write("*");
+    }
+    process.stdout.write("\n");
+  }
+  console.log("\n");
+}
+
+function doublesideIntervertTrianlge() {
+  for (let i = 0; i < 5; i++) {
+    for (let k = 1; k < i + 1; k++) {
+      process.stdout.write("");
+    }
+    for (let j = 0; j < 9 - i * 2; j++) {
+      process.stdout.write("*");
+    }
+
+    process.stdout.write("\n");
+  }
+  console.log("\n");
+}
+
+leftTrianlge();
+leftIntervertTriangle();
+rigthTriangle();
+rigthIntervertTriangle();
+doublesideTrianlge();
+doublesideIntervertTrianlge();
